5737a98fa86688eaebaa04fa7d318ce479bf482c
kscotteng <kscott@gmail.com>
2016-03-06 21:47:41 -0500

    edit fixture data, create additional fixture data

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160305174235) do
-
-  create_table "exercise_types", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
-  end
-# ActiveRecord::Schema.define(version: 20160305174234) do
 
   create_table "cals", force: :cascade do |t|
     t.integer  "cals_consumed"
     t.date     "date"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
 
+  create_table "exercise_types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "steps", force: :cascade do |t|
