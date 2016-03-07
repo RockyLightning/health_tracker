81a873d1352ffec8eae734cce0ea7110316e4369
mkg1 <mkgehring@Mollys-MacBook-Pro.local>
2016-03-06 22:51:39 -0500

    Add method to extract cals_burned

--- app/models/cal.rb
+++ app/models/cal.rb
@@ -1,3 +1,11 @@
 class Cal < ActiveRecord::Base
+  def cal_intake
+    today = Time.now.strftime("%Y-%m-%d")
+    intake = Cal.where(:date => today)
+    intake.sum(:cals_consumed)
+  end
 
+  def stats
+    {intake: cal_intake}
+  end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,13 @@
 class Workout < ActiveRecord::Base
   belongs_to :exercise_type
+
+  def burned
+    today = Time.now.strftime("%Y-%m-%d")
+    burned = Workout.where(:date => today)
+    burned.sum(:cals_burned)
+  end
+
+  def stats
+    {burned: burned}
+  end
 end
