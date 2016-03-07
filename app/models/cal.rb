5482b7330a0483748c3a6ea6535edd33412909c7
mkg1 <mkgehring@Mollys-MacBook-Pro.local>
2016-03-06 23:10:00 -0500

    Add method to calculate average steps taken

--- app/models/cal.rb
+++ app/models/cal.rb
@@ -1,11 +1,11 @@
 class Cal < ActiveRecord::Base
-  def cal_intake
+  def intake
     today = Time.now.strftime("%Y-%m-%d")
     intake = Cal.where(:date => today)
     intake.sum(:cals_consumed)
   end
 
   def stats
-    {intake: cal_intake}
+    {intake: intake}
   end
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,2 +1,10 @@
 class Step < ActiveRecord::Base
+  def avg_steps
+    s = Step.sum(:steps_taken)
+    avg = s/Step.count
+  end
+
+  def stats
+    {avg_steps: avg_steps}
+  end
 end
