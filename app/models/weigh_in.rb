797e2226b553f81c62fbdab2a6d6c82d06731944
mkg1 <mkgehring@Mollys-MacBook-Pro.local>
2016-03-06 23:15:14 -0500

    Add method to find weight change #shipit

--- app/models/weigh_in.rb
+++ app/models/weigh_in.rb
@@ -1,3 +1,13 @@
 class WeighIn < ActiveRecord::Base
   validates :date, presence: true, uniqueness: true
+
+  def weight_change
+    low_weight = WeighIn.minimum(:weight)
+    high_weight = WeighIn.maximum(:weight)
+    diff = high_weight - low_weight
+  end
+
+  def stats
+    {diff: weight_change}
+  end
 end
